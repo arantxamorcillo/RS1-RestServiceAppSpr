4414e1aba8970882993df4eb42ee1ac781497973
Arantxa Morcillo <arantxa.morcillo@gmail.com>
2022-01-25 14:40:55 +0100

    Post petition complete

--- src/main/java/com/example/restservice/GreetingController.java
+++ src/main/java/com/example/restservice/GreetingController.java
@@ -1,9 +1,7 @@
 package com.example.restservice;
 import java.util.concurrent.atomic.AtomicLong;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
@@ -15,6 +13,16 @@ public class GreetingController {
     @GetMapping("/greeting")
     public Greeting greeting(@RequestParam(value = "name", defaultValue = "world") String name){
         return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    }
+
+    @PostMapping("/post")
+    public Greeting greeting(@RequestBody Greeting greeting){
+        return greeting;
+    }
+
+    @PutMapping("/put")
+    public Greeting greeting(@RequestParam int var1,int var2){
+
     }
 
 
